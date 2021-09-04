46aaaaaa4a972fff8fd58871cf93689f90071fd4
Jean-François Senécal <jeanjeansenecal@gmail.com>
2021-09-04 00:09:11 -0400

    User is able to log in his profile

--- client/src/reducers/auth.js
+++ client/src/reducers/auth.js
@@ -3,6 +3,8 @@ import {
   REGISTER_FAIL,
   USER_LOADED,
   AUTH_ERROR,
+  LOGIN_SUCCESS,
+  LOGIN_FAIL,
 } from '../actions/types';
 
 const initialState = {
@@ -23,10 +25,12 @@ export default function (state = initialState, action) {
         loading: false,
         user: payload,
       };
+    case LOGIN_SUCCESS:
     case REGISTER_SUCCESS:
       localStorage.setItem('token', payload.token);
       return { ...state, ...payload, isAuthenticated: true, loading: false };
     case AUTH_ERROR:
+    case LOGIN_FAIL:
     case REGISTER_FAIL:
       localStorage.removeItem('token');
       return {
