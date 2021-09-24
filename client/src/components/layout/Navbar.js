63559c47790c646ea0c3fa398b929db50ac37ef8
Jean-François Senécal <jeanjeansenecal@gmail.com>
2021-09-24 19:38:19 -0400

    added comment to UI, like and post

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -11,6 +11,9 @@ const Navbar = ({ auth: { isAuthenticated, loading }, logout }) => {
         <Link to='/profiles'>Developpers</Link>
       </li>
       <li>
+        <Link to='/posts'>Posts</Link>
+      </li>
+      <li>
         <Link to='/dashboard'>
           <i className='fas fa-user-alt'></i>
           <span className='hide-sm'>Dashboard</span>
